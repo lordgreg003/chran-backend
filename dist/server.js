c82492bf0d51494d51caaf45c6e33dfa6cc30476
chidera nwokoye <chideranwokoye555@gmail.com>
2024-11-14 16:20:08 +0100

    fixed issues

--- dist/server.js
+++ dist/server.js
@@ -1,1 +1,56 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv = __importStar(require("dotenv"));
+dotenv.config();
+const cors_1 = __importDefault(require("cors"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const db_1 = __importDefault(require("./config/db"));
+const blogRoutes_1 = __importDefault(require("./routes/blogRoutes"));
+const adminRoutes_1 = __importDefault(require("./routes/adminRoutes"));
+const errorMiddleware_1 = __importDefault(require("./middlewares/errorMiddleware"));
+// Connect to the database
+(0, db_1.default)();
+// Create Express app
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
+app.use(body_parser_1.default.json());
+// Use blog routes
+app.use("/api/blogs", blogRoutes_1.default);
+app.use("/admin/", adminRoutes_1.default);
+// Error handling middleware
+app.use(errorMiddleware_1.default.notFound);
+app.use(errorMiddleware_1.default.errorHandler);
+// Set up the port for deployment
+const PORT = Number(process.env.PORT) || 3001;
+// Start the server
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server is running on http://0.0.0.0:${PORT}`);
+});
